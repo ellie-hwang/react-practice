7e2275c0de546035c5f09dc001373d04412ea976
Eleonora Hwang <eleonorahwang@gmail.com>
2022-05-19 20:50:37 -0700

    Completed react=hooks-stopwatch

---
+++ react-hooks-stopwatch/src/stopwatch.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+
+function StopWatch() {
+  const [isTicking, setTicking] = useState(false);
+  const [secondsElapsed, setSecondsElapsed] = useState(0);
+
+  useEffect(() => {
+    let intId = null;
+    if (isTicking === true) {
+      intId = setInterval(() => {
+        setSecondsElapsed(secondsElapsed => secondsElapsed + 1)
+      }, 1000);
+    } else if (isTicking === false) {
+      clearInterval(intId);
+    }
+    return () => clearInterval(intId);
+  }, [isTicking, secondsElapsed]);
+
+  function reset() {
+    if (isTicking === false) {
+      setSecondsElapsed(0);
+    }
+  }
+
+  const icon = isTicking ? 'fas fa-pause' : 'fas fa-play';
+  return (
+    <div className="row">
+      <div id="watchface" onClick={reset}>
+        <p>{secondsElapsed}</p>
+      </div>
+      <div id="icon-container">
+        <i onClick={() => setTicking(!isTicking)} className={icon} />
+      </div>
+    </div>
+  );
+
+}
+
+export default StopWatch;
