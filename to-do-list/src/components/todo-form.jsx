b5733bfe5ca2aa8eb16d355ebde6fbf6316d5897
Eleonora Hwang <eleonorahwang@gmail.com>
2022-05-28 21:08:21 -0700

    Created form component for to-do-list

--- to-do-list/src/components/todo-form.jsx
+++ to-do-list/src/components/todo-form.jsx
@@ -1,7 +1,14 @@
 import React, { useState, useEffect } from 'react';
 
 function TodoList() {
-
+  return (
+    <>
+      <form>
+        <input id="todo-item" type="text" value=""></input>
+        <button type="button">ADD</button>
+      </form>
+    </>
+  )
 }
 
 export default TodoList;
--- to-do-list/src/todo-app.jsx
+++ to-do-list/src/todo-app.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import TodoForm from 'components/todo-form';
-import TodoList from 'components/todo-list';
+import TodoForm from './components/todo-form';
+import TodoList from './components/todo-list';
 
 function TodoApp() {
-
+  return (
+    <TodoForm />
+  )
 }
 
 export default TodoApp;
