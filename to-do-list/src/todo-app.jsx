d1fcd77d5fc75cc6c91ba2ec5f93df7fa60c7a90
Eleonora Hwang <eleonorahwang@gmail.com>
2022-06-01 13:56:19 -0700

    New task can be added on submit

--- to-do-list/src/todo-app.jsx
+++ to-do-list/src/todo-app.jsx
@@ -1,15 +1,68 @@
 import React, { useState, useEffect } from 'react';
-import TodoForm from './components/todo-form';
-import TodoList from './components/todo-list';
+
+function Todo({ todo }) {
+  return (
+    <div className="todo px-2 py-1 mb-2">
+      {todo.text}
+    </div>
+  );
+};
+
+function TodoForm({ addTodo }) {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  }
+
+  return (
+    <form className="row mb-2" onSubmit={handleSubmit}>
+      <div className="input-group">
+        <input autoFocus className="form-control" type="text" value={value} onChange={e => setValue(e.target.value)} placeholder="Add a task..."></input>
+        <div className="input-group-append">
+          <button className="btn btn-primary" type="submit">ADD</button>
+        </div>
+      </div>
+    </form>
+  )
+}
 
 function TodoApp() {
+  const [todos, setTodos] = useState([
+    {
+      text: "Feed doggie"
+    },
+    {
+      text: "Buy groceries"
+    },
+    {
+      text: "Do homework"
+    }
+  ]);
+
+  const addTodo = (text) => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
   return (
     <div className="container-fluid">
       <div className="row d-flex justify-content-center">
         <div className="col-12 col-sm-8">
-          <h2 className="text-center"><strong>Todo List</strong></h2>
-          <TodoForm />
-          <TodoList />
+          <div>
+            <div className="todo-list p-2">
+              <h2 className="text-center"><strong>Todo List</strong></h2>
+              <TodoForm addTodo={addTodo} />
+              {
+                todos.map((todo, index) => (
+                  <Todo key={index} index={index} todo={todo} />
+                ))
+              }
+            </div>
+          </div>
         </div>
       </div>
     </div>
