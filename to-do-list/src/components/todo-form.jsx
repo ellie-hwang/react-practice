30d04eac97335d46bd21eb3c5833c6453ed33096
Eleonora Hwang <eleonorahwang@gmail.com>
2022-05-31 23:01:12 -0700

    Added styling to form control and inserted dummy data for todo list

--- to-do-list/src/components/todo-form.jsx
+++ to-do-list/src/components/todo-form.jsx
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from 'react';
 
-function TodoList() {
+function TodoForm() {
   return (
-    <>
-      <form>
-        <input id="todo-item" type="text" value=""></input>
-        <button type="button">ADD</button>
-      </form>
-    </>
+    <form className="row">
+      <div className="input-group">
+        <input autoFocus className="form-control" type="text" value="" placeholder="Add a task..."></input>
+        <div className="input-group-append">
+          <button className="btn" type="submit">ADD</button>
+        </div>
+      </div>
+    </form>
   )
 }
 
-export default TodoList;
+export default TodoForm;
--- to-do-list/src/todo-app.jsx
+++ to-do-list/src/todo-app.jsx
@@ -4,7 +4,15 @@ import TodoList from './components/todo-list';
 
 function TodoApp() {
   return (
-    <TodoForm />
+    <div className="container-fluid">
+      <div className="row d-flex justify-content-center">
+        <div className="col-12 col-sm-8">
+          <h2 className="text-center"><strong>Todo List</strong></h2>
+          <TodoForm />
+          <TodoList />
+        </div>
+      </div>
+    </div>
   )
 }
 
